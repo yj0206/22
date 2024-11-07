bfa479894f1f17732473ce22b500bd5b8fa61421
yj0206 <2452665078@qq.com>
2024-11-07 20:57:33 +0800

    Add files via upload

---
+++ 001/treasureMap.js
@@ -0,0 +1,52 @@
+class TreasureMap {
+    static getInitialClue() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve("在古老的图书馆里找到了第一个线索...");
+            }, 1000);
+        });
+    }
+
+    static decodeAncientScript(clue) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (!clue) {
+                    reject("没有线索可以解码!");
+                }
+                resolve("解码成功!宝藏在一座古老的神庙中...");
+            }, 1500);
+        });
+    }
+
+    static solvePuzzle() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const random = Math.random();
+                if (random < 0.5) {
+                    reject("糟糕!解谜失败...");
+                }
+                resolve("解谜成功!你找到了通往神庙的钥匙...");
+            }, 2000);
+        });
+    }
+
+    static searchTemple(location) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const random = Math.random();
+                if (random < 0.5) {
+                    reject("糟糕!遇到了神庙守卫!");
+                }
+                resolve("找到了一个神秘的箱子...");
+            }, 2000);
+        });
+    }
+
+    static openTreasureBox() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve("恭喜!你找到了传说中的宝藏!");
+            }, 1000);
+        });
+    }
+}
